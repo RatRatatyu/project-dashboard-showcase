0322ffab8ebf10d2b072723219d0272754554b1e
Margarita <kkom.margarita@gmail.com>
2026-09-08 22:57:41 +0500

    Add FakeUserDataProvider.kt with mock user data

---
+++ app/src/main/java/com/example/projectdashboardshowcase/core/data/datasource/FakeUserDataProvider.kt
@@ -0,0 +1,12 @@
+package com.example.projectdashboardshowcase.core.data.datasource
+
+import com.example.projectdashboardshowcase.core.domain.model.User
+
+// We use this object to fake user's data from DataStore
+object FakeUserDataProvider {
+    fun getMockUser(): User = User(
+            id = 100,
+            name = "Анна",
+            avatarUrl = null
+        )
+}
