a1e5fac715503135a6b26cabe2df2f50155c395d
Margarita <kkom.margarita@gmail.com>
2026-09-09 23:44:17 +0500

    Update getDashboardData to return Flow in FakeDashboardRepositoryImpl

--- app/src/main/java/com/example/projectdashboardshowcase/core/data/repository/FakeDashboardRepositoryImpl.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/core/data/repository/FakeDashboardRepositoryImpl.kt
@@ -4,15 +4,19 @@ import com.example.projectdashboardshowcase.core.data.datasource.FakeDashboardDa
 import com.example.projectdashboardshowcase.core.domain.model.DashboardData
 import com.example.projectdashboardshowcase.core.domain.repository.DashboardRepository
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import kotlin.time.Duration.Companion.milliseconds
 
 class FakeDashboardRepositoryImpl @Inject constructor(): DashboardRepository {
 
-    override suspend fun getDashboardData(userId: Int): DashboardData {
-        delay(60.milliseconds)
-        return FakeDashboardDataProvider.getMockData(userId)
-    }
+    override fun getDashboardData(userId: Int): Flow<DashboardData> =
+        flow{
+            delay(60.milliseconds)
+            emit(FakeDashboardDataProvider.getMockData(userId))
+        }
+
 
     override fun updateData() {
         //TODO
