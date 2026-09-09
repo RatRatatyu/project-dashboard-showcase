6c6d7999d04fa6375ba8cff83ab2c227f0933b31
Margarita <kkom.margarita@gmail.com>
2026-09-10 00:03:34 +0500

    Update FakeUserDataProvider with mock avatar resource

--- app/src/main/java/com/example/projectdashboardshowcase/core/data/datasource/FakeUserDataProvider.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/core/data/datasource/FakeUserDataProvider.kt
@@ -1,12 +1,13 @@
 package com.example.projectdashboardshowcase.core.data.datasource
 
 import com.example.projectdashboardshowcase.core.domain.model.User
+import com.example.projectdashboardshowcase.R
 
 // We use this object to fake user's data from DataStore
 object FakeUserDataProvider {
     fun getMockUser(): User = User(
             id = 100,
             name = "Анна",
-            avatarUrl = null
+            avatarUrl = R.drawable.user1
         )
 }
