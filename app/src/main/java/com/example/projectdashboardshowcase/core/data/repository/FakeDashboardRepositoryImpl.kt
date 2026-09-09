ca141d7a0400744fe33fe977bbe58061d2da43a1
Margarita <kkom.margarita@gmail.com>
2026-09-10 00:03:25 +0500

    Increase delay in FakeDashboardRepositoryImpl to 1 second

--- app/src/main/java/com/example/projectdashboardshowcase/core/data/repository/FakeDashboardRepositoryImpl.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/core/data/repository/FakeDashboardRepositoryImpl.kt
@@ -8,12 +8,13 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 class FakeDashboardRepositoryImpl @Inject constructor(): DashboardRepository {
 
     override fun getDashboardData(userId: Int): Flow<DashboardData> =
         flow{
-            delay(60.milliseconds)
+            delay(1.seconds)
             emit(FakeDashboardDataProvider.getMockData(userId))
         }
 
