1b07ed43bc633b89d9f397604599358752b84af8
Margarita <kkom.margarita@gmail.com>
2026-09-10 00:03:16 +0500

    Add statusBarsPadding and fixed height to DashboardTopAppBar

--- app/src/main/java/com/example/projectdashboardshowcase/presentation/mainScreen/components/DashboardTopAppBar.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/presentation/mainScreen/components/DashboardTopAppBar.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
@@ -43,6 +44,8 @@ fun DashboardTopAppBar(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .statusBarsPadding()
+            .height(64.dp)
             .padding(horizontal = 20.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
