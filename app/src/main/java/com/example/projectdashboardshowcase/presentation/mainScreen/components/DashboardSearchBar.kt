f016b0209a8e3ecc0393aea9af557dc3ea73e4e2
Margarita <kkom.margarita@gmail.com>
2026-09-11 00:18:38 +0500

    Update DashboardSearchBar to use Tune icon and string resources

--- app/src/main/java/com/example/projectdashboardshowcase/presentation/mainScreen/components/DashboardSearchBar.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/presentation/mainScreen/components/DashboardSearchBar.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Tune
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButtonDefaults
@@ -17,10 +17,12 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.projectdashboardshowcase.ui.theme.ProjectDashboardShowcaseTheme
+import com.example.projectdashboardshowcase.R
 
 
 @Composable
@@ -37,7 +39,7 @@ fun DashboardSearchBar(
             .fillMaxWidth(),
         placeholder = {
             Text(
-                text = "Поиск по проектам, тикетам и тегам",
+                text = stringResource(R.string.default_searchbar_text),
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
             )
@@ -59,8 +61,8 @@ fun DashboardSearchBar(
                 shape = RoundedCornerShape(8.dp)
             ) {
                 Icon(
-                    imageVector = Icons.Default.FilterList,
-                    contentDescription = "Поиск по фильтрам"
+                    imageVector = Icons.Default.Tune,
+                    contentDescription = stringResource(R.string.search_by_filters)
                 )
             }
         },
