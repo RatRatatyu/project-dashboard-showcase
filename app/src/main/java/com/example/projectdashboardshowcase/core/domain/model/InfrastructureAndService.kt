3d1f8aa1d16004d331f93e3183bc1fb6f8a5eb88
Margarita <kkom.margarita@gmail.com>
2026-09-10 22:36:58 +0500

    Add default value for imageUrl in InfrastructureAndService.kt

--- app/src/main/java/com/example/projectdashboardshowcase/core/domain/model/InfrastructureAndService.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/core/domain/model/InfrastructureAndService.kt
@@ -2,7 +2,7 @@ package com.example.projectdashboardshowcase.core.domain.model
 
 data class InfrastructureAndService(
     val id: Int,
-    val imageUrl: Int?,
+    val imageUrl: Int? = null,
     val name: String,
     val description: String,
     val priority: TaskPriority,
