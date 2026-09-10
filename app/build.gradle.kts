5df6f8841065df523b6d78e9199c4876682d2b24
Margarita <kkom.margarita@gmail.com>
2026-09-10 22:35:25 +0500

    Add Compose UI Text, UI Graphics, and Material Icons Extended dependencies to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.compose.ui.text)
+    implementation(libs.androidx.ui.graphics)
     coreLibraryDesugaring(libs.desugar.jdk.libs)
     ksp(libs.hilt.compiler)
     implementation(libs.androidx.hilt.common)
@@ -50,6 +52,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
