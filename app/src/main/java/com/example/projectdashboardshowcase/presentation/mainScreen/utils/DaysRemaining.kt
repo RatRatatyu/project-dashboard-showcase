9fbd7a595a2fc80c9ea9aceaa467eafa5728db3d
Margarita <kkom.margarita@gmail.com>
2026-09-12 18:43:19 +0500

    Update DaysRemaining.kt with string resources and pluralization for days and months

--- app/src/main/java/com/example/projectdashboardshowcase/presentation/mainScreen/utils/DaysRemaining.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/presentation/mainScreen/utils/DaysRemaining.kt
@@ -1,17 +1,42 @@
 package com.example.projectdashboardshowcase.presentation.mainScreen.utils
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
+import com.example.projectdashboardshowcase.R
 
 
+@Composable
 fun getDaysRemainingText(dueDate: LocalDate): String {
     val today = LocalDate.now()
+
     val daysBetween = ChronoUnit.DAYS.between(today, dueDate)
+    val monthsBetween = ChronoUnit.MONTHS.between(today, dueDate)
 
     return when {
-        daysBetween == 0L -> "Сегодня"
-        daysBetween == 1L -> "Завтра"
-        daysBetween > 1L -> "Осталось $daysBetween дн."
-        else -> "Просрочено"
+        daysBetween < 0L -> stringResource(R.string.overdue)
+        daysBetween == 0L -> stringResource(R.string.today)
+        daysBetween == 1L -> stringResource(R.string.tomorrow)
+
+
+        monthsBetween >= 1L -> {
+            val months = monthsBetween.toInt()
+            pluralStringResource(
+                id = R.plurals.months_remaining,
+                count = months,
+                months
+            )
+        }
+
+        else -> {
+            val days = daysBetween.toInt()
+            pluralStringResource(
+                id = R.plurals.days_remaining,
+                count = days,
+                days
+            )
+        }
     }
 }
