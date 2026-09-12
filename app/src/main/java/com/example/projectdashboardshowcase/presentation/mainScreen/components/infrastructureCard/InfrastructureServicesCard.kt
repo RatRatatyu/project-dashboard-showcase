732e65296c0cc6b665e8e0fbead97a34db326b25
Margarita <kkom.margarita@gmail.com>
2026-09-12 18:46:21 +0500

    Remove unused CardDefaults and MaterialTheme imports in InfrastructureServicesCard.kt

--- app/src/main/java/com/example/projectdashboardshowcase/presentation/mainScreen/components/infrastructureCard/InfrastructureServicesCard.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/presentation/mainScreen/components/infrastructureCard/InfrastructureServicesCard.kt
@@ -3,8 +3,6 @@ package com.example.projectdashboardshowcase.presentation.mainScreen.components.
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
