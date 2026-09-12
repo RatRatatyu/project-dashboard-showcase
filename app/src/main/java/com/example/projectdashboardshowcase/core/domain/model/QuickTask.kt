a38d292f672bceab49643cd44193ea6bc9bad60d
Margarita <kkom.margarita@gmail.com>
2026-09-12 18:45:09 +0500

    Update QuickTask.kt with LocalDateTime for dueDate

--- app/src/main/java/com/example/projectdashboardshowcase/core/domain/model/QuickTask.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/core/domain/model/QuickTask.kt
@@ -1,11 +1,13 @@
 package com.example.projectdashboardshowcase.core.domain.model
 
+import java.time.LocalDateTime
+
 data class QuickTask(
     val id: Int,
     val title: String,
     val isCompleted: Boolean,
     val priority: TaskPriority, // enum P1, P2, P3
-    val dueDate: String,
+    val dueDate: LocalDateTime,
     val category: String
 )
 
