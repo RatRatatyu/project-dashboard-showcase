4d26f95087e651a4d34003f4ae631394c8a9edfd
Margarita <kkom.margarita@gmail.com>
2026-09-13 13:47:48 +0500

    Add isBookmarked property to ProjectInFocus data class

--- app/src/main/java/com/example/projectdashboardshowcase/core/domain/model/ProjectInFocus.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/core/domain/model/ProjectInFocus.kt
@@ -5,6 +5,7 @@ import java.time.LocalDate
 data class ProjectInFocus (
     val id: Int,
     val imageUrl: Int? = null,
+    val isBookmarked: Boolean,
     val dueDate: LocalDate,
     val categoryHub: String,
     val name: String,
