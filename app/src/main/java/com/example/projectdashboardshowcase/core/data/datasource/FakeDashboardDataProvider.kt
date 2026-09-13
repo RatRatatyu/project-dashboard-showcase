58646d28c3f29344b94b4312f42dae9b9ed13d96
Margarita <kkom.margarita@gmail.com>
2026-09-13 13:46:53 +0500

    Update FakeDashboardDataProvider.kt with observeMockData and toggleState bookmark functionality

--- app/src/main/java/com/example/projectdashboardshowcase/core/data/datasource/FakeDashboardDataProvider.kt
+++ app/src/main/java/com/example/projectdashboardshowcase/core/data/datasource/FakeDashboardDataProvider.kt
@@ -1,11 +1,35 @@
 package com.example.projectdashboardshowcase.core.data.datasource
 
-import com.example.projectdashboardshowcase.core.domain.model.*
+import android.util.Log
+import com.example.projectdashboardshowcase.core.domain.model.DashboardData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
 
 
 // We use this object to fake dashboard data from server
 object FakeDashboardDataProvider {
-    fun getMockData(userId: Int): DashboardData {
-        return data.firstOrNull { it.userId == userId } ?: data.first()
+
+    private val _localData = MutableStateFlow(data)
+
+    fun observeMockData(userId: Int): Flow<DashboardData> {
+        return _localData.map { list ->
+            list.firstOrNull { it.userId == userId } ?: list.first()
+        }
+    }
+
+    fun toggleState(state: Boolean, userId: Int){
+        val currentList = _localData.value.toMutableList()
+
+        val index = currentList.indexOfFirst { it.userId == userId }
+
+        if (index != -1) {
+            currentList[index] = currentList[index].copy(
+                projectInFocus = currentList[index].projectInFocus.copy(isBookmarked = state)
+            )
+
+            _localData.value = currentList
+        }
+        Log.i("UPDATE_CHECK", "isBooked update to $state")
     }
 }
